41d194a15f72bb4fadcbe0baa195f0baca6b0fda
Liu-Steve <steve_liu@whu.edu.cn>
2022-06-03 02:16:54 +0800

    finish the cart without sqlite

--- app/src/main/java/com/example/bookshop/CartAdapter.java
+++ app/src/main/java/com/example/bookshop/CartAdapter.java
@@ -2,6 +2,7 @@ package com.example.bookshop;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,16 +14,40 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CartAdapter extends ArrayAdapter<CartBean> {
 
     private ViewHolder viewHolder;
+    private final ArrayList<ViewHolder> viewHolderMap;
     private final int imageId;
+    private final CartFragment fragment;
+    private final List<CartBean> obj;
 
-    public CartAdapter(@NonNull Context context, int resource, @NonNull List<CartBean> objects) {
+    @Override
+    public int getCount() {
+        return obj.size();
+    }
+
+    @Override
+    public CartBean getItem(int position) {
+        return obj.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public CartAdapter(@NonNull Context context, int resource, @NonNull List<CartBean> objects, CartFragment fragment) {
         super(context, resource, objects);
         this.imageId = resource;
+        this.fragment = fragment;
+        this.obj = objects;
+        this.viewHolderMap = new ArrayList<>();
     }
 
     public static class ViewHolder {
@@ -33,7 +58,6 @@ public class CartAdapter extends ArrayAdapter<CartBean> {
         public Button addBtn;
         public Button subBtn;
         public TextView sumPrice;
-        public Button deleteBtn;
         public Button buyBtn;
     }
 
@@ -42,26 +66,59 @@ public class CartAdapter extends ArrayAdapter<CartBean> {
     @Override
     public View getView(final int position, View convertView, ViewGroup parent)
     {
+        DecimalFormat formatter = new DecimalFormat("0.00");
+
         if (null != convertView)
         {
             viewHolder = (ViewHolder) convertView.getTag();
-            return convertView;
+            //return convertView;
+        } else {
+            viewHolder = new ViewHolder();
+            viewHolderMap.add(viewHolder);
+            convertView = LayoutInflater.from(getContext()).inflate(imageId, parent, false);
+
+            viewHolder.bookCover = convertView.findViewById(R.id.book_cover_cart);
+            viewHolder.bookTitle = convertView.findViewById(R.id.book_title_cart);
+            viewHolder.price = convertView.findViewById(R.id.price_cart);
+            viewHolder.count = convertView.findViewById(R.id.count_cart);
+            viewHolder.sumPrice = convertView.findViewById(R.id.sum_price_cart);
+            viewHolder.addBtn = convertView.findViewById(R.id.add_btn_cart);
+            viewHolder.subBtn = convertView.findViewById(R.id.sub_btn_cart);
+            viewHolder.buyBtn = convertView.findViewById(R.id.buy_cart);
+
+            viewHolder.addBtn.setOnClickListener(view -> {
+                CartBean item = getItem(position);
+                int count = item.getCount();
+                item.setCount(count + 1);
+                ViewHolder newView = viewHolderMap.get(position);
+                newView.count.setText(String.valueOf(item.getCount()));
+                float newSumPrice = item.getPrice() * item.getCount() - item.getDiscount();
+                newView.sumPrice.setText(formatter.format(newSumPrice));
+                notifyDataSetChanged();
+            });
+
+            viewHolder.subBtn.setOnClickListener(view -> {
+                CartBean item = getItem(position);
+                int count = item.getCount();
+                item.setCount(count - 1);
+                ViewHolder newView = viewHolderMap.get(position);
+                newView.count.setText(String.valueOf(item.getCount()));
+                float newSumPrice = item.getPrice() * item.getCount() - item.getDiscount();
+                newView.sumPrice.setText(formatter.format(newSumPrice));
+                if (count <= 1)
+                {
+                    obj.remove(position);
+                    viewHolderMap.remove(position);
+                }
+                notifyDataSetChanged();
+            });
+
+            viewHolder.buyBtn.setOnClickListener(view -> fragment.showDialog());
+
+            convertView.setTag(viewHolder);
         }
 
-        viewHolder = new ViewHolder();
-        CartBean bean = getItem(position);
-        convertView = LayoutInflater.from(getContext()).inflate(imageId, parent, false);
-
-        DecimalFormat formatter = new DecimalFormat("0.00");
-
-        viewHolder.bookCover = convertView.findViewById(R.id.book_cover_cart);
-        viewHolder.bookTitle = convertView.findViewById(R.id.book_title_cart);
-        viewHolder.price = convertView.findViewById(R.id.price_cart);
-        viewHolder.count = convertView.findViewById(R.id.count_cart);
-        viewHolder.sumPrice = convertView.findViewById(R.id.sum_price_cart);
-        viewHolder.addBtn = convertView.findViewById(R.id.add_btn_cart);
-        viewHolder.subBtn = convertView.findViewById(R.id.sub_btn_cart);
-        viewHolder.buyBtn = convertView.findViewById(R.id.buy_cart);
+        CartBean bean = obj.get(position);
 
         viewHolder.bookCover.setImageResource(bean.getImageID());
         viewHolder.bookTitle.setText(bean.getName());
@@ -69,8 +126,6 @@ public class CartAdapter extends ArrayAdapter<CartBean> {
         viewHolder.count.setText(String.valueOf(bean.getCount()));
         float sumPrice = bean.getPrice() * bean.getCount() - bean.getDiscount();
         viewHolder.sumPrice.setText(formatter.format(sumPrice));
-
-        convertView.setTag(viewHolder);
         return convertView;
     }
 
--- app/src/main/java/com/example/bookshop/CartFragment.java
+++ app/src/main/java/com/example/bookshop/CartFragment.java
@@ -1,15 +1,20 @@
 package com.example.bookshop;
 
+import static androidx.core.content.res.TypedArrayUtils.getString;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,18 +40,47 @@ public class CartFragment extends Fragment {
         ListView listView = (ListView)getView().findViewById(R.id.listview);
         List<CartBean> list=new ArrayList<>();
         init(list);
-        //HistoryAdapter historyAdapter=new HistoryAdapter(getActivity(),R.layout.list,list);
-        CartAdapter cartAdapter = new CartAdapter(getActivity(), R.layout.cart_item, list);
+        CartAdapter cartAdapter = new CartAdapter(getActivity(), R.layout.cart_item, list, this);
         listView.setAdapter(cartAdapter);
     }
 
     private void init(List<CartBean> list)
     {
-        CartBean bean1=new CartBean("玩具",1,R.drawable.wanju,4
-                ,100,0);
-        CartBean bean2=new CartBean("香水",2,R.drawable.xiangshui,5
-                ,200,0);
+        CartBean bean1=new CartBean("人类简史",1,R.drawable.brief_history,4
+                , 15.00F,0);
+        CartBean bean2=new CartBean("追风筝的人",2,R.drawable.kite,5
+                ,20.00F,0);
         list.add(bean1);
         list.add(bean2);
     }
+
+    public void showDialog(){
+        AlertDialog.Builder newDialog = new AlertDialog.Builder(getContext());
+        newDialog.setTitle("网上书店");
+        newDialog.setIcon(R.mipmap.ic_launcher_round);
+        newDialog.setMessage("确认支付？");
+
+        //设置按钮
+        newDialog.setPositiveButton("确定"
+                , new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity()
+                                ,"支付成功，已加入购买历史记录",Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                });
+        newDialog.setNegativeButton("取消"
+                , new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(),
+                                "已取消支付",Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                });
+
+        newDialog.create().show();
+    }
+
 }
